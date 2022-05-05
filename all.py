e16fced81c970d5b335e1b95f1d31548f4415161
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-05-05 20:43:40 +0530

    Video , Assignment , Comment Feature
    Features added

--- all.py
+++
@@ -1,3 +0,0 @@
-from profiles.models import Student
-user=Student.objects.get(id=1)
-print(user.__dict__)
--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Classroom_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,5 +19,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
